63a19952270b9ac56a453762a23beb789e514972
Adam Kjellberg <adamkjellberg@Adams-MacBook-Pro.local>
2021-03-06 14:38:14 +0100

    started with the js

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import './Pokemons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div class = 'main'>
+        <h1>PokeDex</h1>
+        <div id = 'pokeContainer' class = 'poke-container'></div>
+      </div>
+    </main>
   );
 }
 
